7d40f3680d0ce1ae8f706a5c8c7de3ec4fa467cc
tradez <tradezmc@gmail.com>
2019-07-18 02:59:12 +0100

    #1

--- src/io/oaft/colorgame/manager/impl/GameManager.java
+++ src/io/oaft/colorgame/manager/impl/GameManager.java
@@ -2,6 +2,7 @@ package io.oaft.colorgame.manager.impl;
 
 import io.oaft.colorgame.manager.Manager;
 import io.oaft.colorgame.manager.ManagerHandler;
+import io.oaft.colorgame.util.CC;
 import io.oaft.colorgame.util.GameState;
 import io.oaft.colorgame.util.Timer;
 
@@ -9,6 +10,7 @@ public class GameManager extends Manager {
 
     private GameState gameState;
     private Timer startingTimer;
+    private Timer gameTimer;
 
     public GameManager(ManagerHandler managerHandler) {
         super(managerHandler);
@@ -18,10 +20,35 @@ public class GameManager extends Manager {
     private void load() {
         this.gameState = GameState.PRE_GAME;
         this.startingTimer = new Timer(getPlugin(), 10, true);
+        this.gameTimer = new Timer(getPlugin(), 0, false);
     }
 
     public void startGame() {
+        this.startingTimer.start();
+        this.gameState = GameState.PRE_GAME;
+        getScheduler().runTaskTimerAsynchronously(getPlugin(), () -> {
+            if(this.startingTimer.getSeconds() == 10) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 10 seconds.");
+            } else if (this.startingTimer.getSeconds() == 5) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 5 seconds.");
+            } else if (this.startingTimer.getSeconds() == 4) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 4 seconds.");
+            } else if (this.startingTimer.getSeconds() == 3) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 3 seconds.");
+            } else if (this.startingTimer.getSeconds() == 2) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 2 seconds.");
+            } else if (this.startingTimer.getSeconds() == 1) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting in 1 second.");
+            } else if (this.startingTimer.getSeconds() == 0) {
+                getPlugin().getServer().broadcastMessage(CC.GOLD + "Game starting now.");
+                this.doGame();
+                this.startingTimer.stop();
+            }
+        }, 0L, 20L);
+    }
 
+    private void doGame() {
+        this.gameState = GameState.IN_GAME;
     }
 
 }
